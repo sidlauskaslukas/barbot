64d3d96a886840ef8c5572f7a939bddc179a435d
Lukas Sidlauskas <lukas.sidlauskas@gmail.com>
2016-11-07 19:38:47 +0200

    Added hold & wait parameters support, enabled acceleration & deacceleration, increased travel & homing speeds

--- Barbot.ino
+++ Barbot.ino
@@ -25,8 +25,7 @@ void setup() {
   Serial.begin(9600);
   maestroSerial.begin(9600); // Servo controller
   Serial2.begin(9600); // Bluetooth module
-  stepper.setMaxSpeed(500);
-  //stepper.setAcceleration(20); // Turned off because stepper motor skipping steps
+  stepper.setMaxSpeed(1500);
   //stepper.setEnablePin(enablePin); // Doesn't work for unknown reason
   //stepper.enableOutputs();
   pinMode(endStopPin, INPUT_PULLUP); // Initialize endstop pin with the internal pull-up resistor enabled
@@ -38,7 +37,7 @@ void homeXAxis() {
   
   while (endStopState == HIGH) {
     stepper.moveTo(100);
-    stepper.setSpeed(500);
+    stepper.setSpeed(700);
     stepper.runSpeed();
     endStopState = digitalRead(endStopPin);
   }
@@ -122,16 +121,17 @@ void moveXTo(String input) {
 
   Serial.print("X goes to: ");
   Serial.println(pos);
-  
+
   if(pos < 0 && pos >= maxPosition) {
+    stepper.setAcceleration(3000);
     stepper.moveTo(pos);
-    while(stepper.distanceToGo() != 0) {
       if(pos < stepper.currentPosition()) {
-        stepper.setSpeed(-500);
+        stepper.setSpeed(-100);
       } else {
-        stepper.setSpeed(500);
+        stepper.setSpeed(100);
       }
-      stepper.runSpeed(); 
+    while(stepper.distanceToGo() != 0) {
+      stepper.run(); 
     }
   } else {
     Serial.println("Position should be between -4995 and 0");
@@ -139,19 +139,54 @@ void moveXTo(String input) {
 }
 
 void pour(String input) {
-  int times = input.substring(1).toInt();
-  int count = 0;
+  int count = 0; // pour counter
+  int times = 0; // times to pour
+  int holdDuration = 0; // time duration to hold dispenser in open position
+  int waitDuration = 0; // time duration to wait till next pour
+  
+  for(int z=0; z<input.length(); z++) {
+    byte parameter = input.substring(z, z+1).charAt(0);
 
-  for(int i=0; i<times; i++) { 
-    maestro.setTarget(0, 8000);
-    delay(2200);
-    maestro.setTarget(0, 4684);
-    if(times - 1 > count) {
-      delay(3000);
-    } else {
-      delay(400);
+    switch(parameter) {
+      case 'F':
+        times = getParameterValue(input, z);
+        break;
+      case 'H':
+        holdDuration = getParameterValue(input, z);
+        break;
+      case 'W':
+        waitDuration = getParameterValue(input, z);
+        break;
     }
-    count++;
+  }
+
+  if(holdDuration > 0 && waitDuration > 0) {
+    for(int i=0; i<times; i++) {
+      maestro.setSpeed(0, 30);
+      maestro.setTarget(0, 4120);
+      delay(holdDuration);
+      maestro.setSpeed(0, 0);
+      maestro.setTarget(0, 6000);
+      if(times - 1 > count) {
+        delay(waitDuration);
+      } else {
+        delay(600);
+      }
+      count++;
+    }
+  } else {
+    Serial.println("Hold and wait duration parameters cannot be lower than or equal to 0");
   }
 }
 
+int getParameterValue(String input, int z) {
+  for(int y=z+1; y<input.length(); y++) {
+    if(input.substring(y, y+1) == " ") {
+      return input.substring(z+1, y).toInt();
+      break;
+    }
+    if(y == input.length() - 1) {
+      return input.substring(z+1, y+1).toInt();
+    }
+  }
+}
